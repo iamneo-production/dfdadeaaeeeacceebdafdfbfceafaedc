73148cb8cfc57fab384ad4829acc722ca97f9f52
Iamneo <techadmin@iamneo.ai>
2023-08-05 12:06:47 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.examly.springapp.model.User;
-
-public interface UserDAO extends JpaRepository<User, Integer> {
-
-}
